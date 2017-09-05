6641d62b87ace2a197101d77b02dc467b27953f8
Gerald Lovel <glovel@aaltsys.com>
2017-09-05 16:49:30 -0500

    Added Stream filename decoding

--- exchange/stream.sh
+++ exchange/stream.sh
@@ -1,1 +1,13 @@
- 
+   /bin/bash
+   STREAM=$1
+   DROPBOX=~/Dropbox/dermpro/ORDERS
+   cd $DROPBOX
+   (insert check for directory exists, else exit)
+   mkdir ./WORKING
+   touch ./WORKING/orders.jsn
+   mv ./*.JSN ./WORKING
+   ls -al ./WORKING/ >>./STREAMS/$STREAM.ls
+   touch ./WORKING/orders.jsn
+   find ./WORKING -type f -name '*.JSN' -exec cat {} + >> ./WORKING/orders.jsn
+   cp ./WORKING/orders.jsn ./STREAMS/$STREAM.JSN
+   mv ./WORKING/*.JSN ./HISTORY/ 
